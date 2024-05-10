f386438b83e410e3ee561711d7d3e067951b71a2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-05-10 12:36:38 +0100

    creating arrays & using array functions

---
+++ 07-ARRAYS/arrays.js
@@ -0,0 +1,37 @@
+// const friend1 = "shola";
+// const friend2 = "Ade";
+// const friend3 = "Bunmi";
+// const friend4 = "Opeyemi";
+// const friend5 = "Muhammed";
+// const friend6 = "Tope";
+
+//literal array
+// const friends = ['shola', 'Ade', 'Bunmi', 'Opeyemi', 'Muhammed', 'Tope'];
+
+const friends = new Array('shola', 'Ade', 'Bunmi', 'Opeyemi', 'Muhammed', 'Tope');
+
+const enemies = new Array('lucifer', 419, false, "MMM", null, true, "419");
+
+console.log(enemies[6]);
+
+console.log(enemies);
+
+console.log(friends);
+
+// accessing array values via index
+console.log(friends[5]);
+
+console.log(friends.length);
+
+friends[2] = 'Funmilayo';
+
+console.log(friends);
+
+
+//ARRAY METHODS
+const newFriends = friends.push('Beauty');
+
+console.log(friends);
+
+
+
