5031e7d97576ae0cb41a0b4be126fb6e6efbc279
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-05-21 14:37:36 +0100

    javascript objects

---
+++ 08-OBJECTS/objects.js
@@ -0,0 +1,77 @@
+// let dog = ["Snoop Dogg", 2.4, "brown", "chihuahua", 3, true];
+
+//console.log(dog);
+
+
+let dog = {
+    dogName: "Snoop Dogg",
+    weight: 2.4,
+    color: "brown",
+    breed: "chihuahua",
+    age: 3,
+    burglarBiter: true
+};
+
+//console.log(dog);
+
+// accessing properties of the object using the dot(.) operator
+// console.log(dog.dogName);
+// console.log(dog.age);
+
+// accessing properties of the object using the array([""]) operator
+// console.log(dog["color"]);
+// console.log(dog["weight"]);
+
+//updating our object
+// dog.dogName = "Nate Dogg";
+// dog["burglarBiter"] = false;
+
+// console.log(dog);
+
+// let variable = "age";
+// console.log(dog[variable]);
+
+// variable = "breed";
+// console.log(dog[variable]);
+
+// creating object inside an object
+// let company = {
+//     companyName: "Dangote",
+//     activities: ["manufacturing", "manufacturing toys", "improving kids health"],
+//     address: {
+//         street: "Falomo Bridge",
+//         number: 12,
+//         zipcode: 33116,
+//         city: "Ikoyi",
+//         state: "Lagos"
+//     },
+//     yearOfEstablishment: 2021
+// };
+// console.log(company);
+// console.log(typeof company.address.number);
+// console.log(company.address.city);
+
+// company.address.number = 15;
+
+// console.log(company.activities[2]);
+
+
+// creating an array of objects
+// let addresses = [{
+//         street: "2nd street",
+//         number: "123",
+//         zipcode: "33116",
+//         city: "Miami",
+//         state: "Florida"
+//     },
+//     {
+//         street: "1st West avenue",
+//         number: "5",
+//         zipcode: "75001",
+//         city: "Addison",
+//         state: "Texas"
+//     }];
+
+// console.log(addresses[1].street);
+
+
